a6c0743fa471fbf625693b49b896ba4f8d541a55
DominicEliot <eliotd@gmail.com>
2026-07-16 09:32:40 -0700

    feat: automatically fetch the codex data

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ This project can be used to generate markdown files for all sorcery codex data.
 ### Running the project:
 Simply run the included js file with nodejs
 
-### Updating the source json:
+### Updating the source json manually (no longer required):
 To update the underlying json file the following manual process needs to be performed:
 1. Open https://curiosa.io/codex
 2. Open your browser's developers tools (typically F12)
@@ -11,5 +11,6 @@ To update the underlying json file the following manual process needs to be perf
 4. Paste the contents into a text editor, preferibly with a json formatter like vs code
 5. replace the codex.json files with the entire "data" object (close/fold it to make this faster)
 6. Make sure to remove the `"data":` property so that your file starts `[` and ends with `]`, and no comma after the last ]
+7. Edit the codex-parser.js to use the file (see comment towards top of main function)
 
 _Note: Firefox users need to enable large response payloads to see the data in the inspection tool. This can be done be opening the `about:config` page and setting `devtools.netmonitor.responseBodyLimit` to a much higher value, or 0 to disable it all together._
--- codex-parser.js
+++ codex-parser.js
@@ -1,31 +1,54 @@
 import * as fs from 'fs'
-import {portableTextToMarkdown} from '@portabletext/markdown'
+import { portableTextToMarkdown } from '@portabletext/markdown'
 import * as path from 'node:path'
 
-let rawData = fs.readFileSync('codex.json');
-let fullCodex = JSON.parse(rawData);
+async function main() {
+    // Uncomment the following lines to use a file instead, and comment out/delete the GetCodexFromCuriosaIo line.
+    //let rawData = fs.readFileSync('codex.json');
+    //let codexData = JSON.parse(rawData);
+    let codexData = await GetCodexFromCuriosaIo();
 
-const damageGridRegex = /```json(.*\n)*?```/g;
 
-let singleFileMarkdown = "";
+    const damageGridRegex = /```json(.*\n)*?```/g;
 
-for (let codex of fullCodex){
-    let markdown = portableTextToMarkdown(codex.content).replaceAll(damageGridRegex, '_See live codex for table_');
-    markdown = "# " + codex.title + "\n" + markdown;
-    
-    if (codex.subcodexes != null) {
-        for (let subcodex of codex.subcodexes) {
-            let subCodexMd = portableTextToMarkdown(subcodex.content).replaceAll(damageGridRegex, '_See live codex for table_');
-            markdown += "\n\n## " + subcodex.title + "\n" + subCodexMd;
-        };
+    let singleFileMarkdown = "";
+
+    for (let codex of codexData) {
+        let markdown = portableTextToMarkdown(codex.content).replaceAll(damageGridRegex, '_See live codex for table_');
+        markdown = "# " + codex.title + "\n" + markdown;
+
+        if (codex.subcodexes != null) {
+            for (let subcodex of codex.subcodexes) {
+                let subCodexMd = portableTextToMarkdown(subcodex.content).replaceAll(damageGridRegex, '_See live codex for table_');
+                markdown += "\n\n## " + subcodex.title + "\n" + subCodexMd;
+            };
+        }
+
+        markdown += '\n\n[Codex Entry](https://curiosa.io/codex?id=' + codex._id + ')';
+        markdown = markdown.replaceAll(/\n\n\n+/g, '\n\n');
+
+        singleFileMarkdown += markdown + '\n\n';
+
+        fs.writeFileSync(path.join('markdown', codex.title + '.md'), markdown);
     }
 
-    markdown += '\n\n[Codex Entry](https://curiosa.io/codex?id=' + codex._id + ')';
-    markdown = markdown.replaceAll(/\n\n\n+/g, '\n\n');
-
-    singleFileMarkdown += markdown + '\n\n';
-
-    fs.writeFileSync(path.join('markdown', codex.title + '.md'), markdown);
+    fs.writeFileSync(path.join('Full Codex.md'), singleFileMarkdown.trim());
 }
 
-fs.writeFileSync(path.join('Full Codex.md'), singleFileMarkdown.trim());
+main().catch(console.error);
+
+async function GetCodexFromCuriosaIo() {
+    const response = await fetch('https://curiosa.io/codex');
+    if (!response.ok) {
+        throw new Error(`Couldn't load URL. Http Status: ${response.status} - ${response.statusText}`);
+    }
+    const data = await response.text();
+
+    let searchTerm = `<script id="__NEXT_DATA__" type="application/json">`;
+    let start = data.indexOf(searchTerm) + searchTerm.length;
+    let end = data.indexOf(`</script>`, start);
+    let jsonString = data.substring(start, end);
+    let codexMetaData = JSON.parse(jsonString);
+    let codexData = codexMetaData.props.pageProps.trpcState.json.queries[0].state.data;
+    return codexData;
+}
